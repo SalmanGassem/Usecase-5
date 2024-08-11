681ce94be1af401aa04053c1224abd3f18e03692
Salman <s.z.gassem@gmail.com>
2024-08-12 00:11:36 +0300

    Salman Gassem, Day-16, updating my streamlit

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 import streamlit as st
 
 st.title('Mythbusting the Job Search Pessimism')
-st.write("Hello, world!")
 
-# Add more components here as needed
+st.markdown("""
+Ever since sophomore year in University, I’ve been wondering about my next step after I graduate. One thing that stood out for me is how little people talk about where to go for the best job opportunities, and all I heard was how hard it is to get a job.
 
+I have always thought something was off, and not until I saw this dataset that all my doubts were confirmed.
+
+The data I’m about to show you is from Jadarat, a Unified National Employment Platform for the Saudi people to find jobs.
+
+I noticed that the place with the most job opportunities is Riyadh:
+
+The eastern and the western regions also offer great opportunities to get hired.
+
+It’s expected for fresh grads to get no more than 18000 SR Salary.
+
+Another point that I always disliked about spreading misinformation in this topic is that if you had no prior experience, you won't get hired. The graph below contradicts this statement in a big way:
+
+Most of the offerings are asking for fresh graduates, people with 0 years of experience! Such an uplifting view makes me optimistic for the future.
+
+It’s also worth mentioning a fact that I am proud of, Saudi Arabia is rapidly evolving, and what drove this positive change are women. Although the chart below will show you a minor preference towards males, the percentages are almost equal, indicating that both genders are highly active in driving Saudi Arabia to heights never been reached before.
+
+So rest assured that as long as you work hard, and are actively searching, you will for sure find a job.
+
+**Don’t let the muggles get you down!!**
+""")
